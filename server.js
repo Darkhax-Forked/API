29e651887cdf185779442161495ca66b38f9f631
TheCodedOne <dr.kcodex@gmail.com>
2017-04-01 16:17:54 +0000

    consistancy

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 var fs = require("fs");
-var bodyParser = require('body-parser');
-var mysql = require('mysql');
-var promise = require('promise');
-var oauthserver = require('oauth2-server');
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var promise = require("promise");
+var oauthserver = require("oauth2-server");
 
 var settings = JSON.parse(fs.readFileSync("../settings.json"));
 var connection = mysql.createConnection({
@@ -24,21 +24,21 @@ var port = 1234;
 
 var router = express.Router();
 
-router.get('/', function (req, res) {
+router.get("/", function (req, res) {
     res.json({status: 404, error: "Not Found", message: "No version specified."})
 })
 
-router.get('/v1/', function (req, res) {
+router.get("/v1/", function (req, res) {
     res.json({status: 404, error: "Not Found", message: "No such endpoint."})
 })
 
-router.get('/v1/users', function (req, res) {
-    connection.query('SELECT * FROM `USERS`', function (error, results, fields) {
+router.get("/v1/users", function (req, res) {
+    connection.query("SELECT * FROM `USERS`", function (error, results, fields) {
         if (error) res.json({status: 0, error: "", message: ""});
         var users = [];
         for (i = 0; i < results.length; i++) {
             var result = results[i];
-            var user =[];
+            var user = {};
             user["id"]=result["USER_ID"];
             user["created"]=result["CREATED"];
             user["avatar"]=result["AVATAR"];
@@ -48,12 +48,12 @@ router.get('/v1/users', function (req, res) {
     });
 })
 
-router.get('/v1/users/:id', function (req, res) {
+router.get("/v1/users/:id", function (req, res) {
     res.json({status: 404, error: "Not Found", message: req.params.id})
 })
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(port);
 
-console.log('Magic happens on port ' + port);
+console.log("Magic happens on port " + port);
